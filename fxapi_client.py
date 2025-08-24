2f9c8e1912d069dec6adf59c5305bef80ce6aa0f
Donwise123 <donwise1123@gmail.com>
2025-08-25 00:11:40 +0100

    Create fxapi_client.py

---
+++ fxapi_client.py
@@ -0,0 +1,11 @@
+import randomimport random
+
+class FXAPI:
+    def get_price(self, pair):
+        # Dummy: simulate market price
+        return round(1900 + random.uniform(-5, 5), 2)
+
+class FXAPI:
+    def get_price(self, pair):
+        # Dummy: simulate market price
+        return round(1900 + random.uniform(-5, 5), 2)
